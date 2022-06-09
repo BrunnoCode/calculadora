b225ce2fec3480d3a2372b63dcfca7eb46b510a3
Brunno Rocha <brunnowr.bw@gmail.com>
2022-06-09 08:06:10 +0200

    functions-calculadora

--- funcionalidade.js
+++ funcionalidade.js
@@ -1,4 +1,14 @@
 function insert(num)
 {
- document.getElementById('resultado').innerHTML = num;
+var numero = document.getElementById('resultado').innerHTML;
+document.getElementById('resultado').innerHTML = numero + num;
+}
+function clean()
+{
+  document.getElementById('resultado').innerHTML = "";
+}
+function back()
+{
+  var resultado = document.getElementById('resultado').innerHTML;
+  document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
 }
